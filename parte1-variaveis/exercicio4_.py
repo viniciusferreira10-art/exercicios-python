ae4d6c2df7332ef112db252ea3e049387614bcbb
viniciusferreira10 <vinicius_ferreira10@estudante.sesisenai.org.br>
2026-09-17 09:15:13 -0300

    exercicio4

--- parte1-variaveis/exercicio4_.py
+++ parte1-variaveis/exercicio4_.py
@@ -1,4 +1,4 @@
 print("digite uma temperatura em celsius: ")
 celsius = input()
 Fahrenheit = (float(celsius) * 9/5) + 32.5
-print("A temperatura em Fahrenheit é: ", Fahrenheit)
+print("A temperatura em Fahrenheit é: ", f"{Fahrenheit:.2f}")
