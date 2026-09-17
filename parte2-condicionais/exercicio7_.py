5d72181ace38bfaead025927900093a7667b1a4e
viniciusferreira10 <vinicius_ferreira10@estudante.sesisenai.org.br>
2026-09-17 09:25:17 -0300

    exercicio 7

---
+++ parte2-condicionais/exercicio7_.py
@@ -0,0 +1,10 @@
+print("digite um número: ")
+numero = int(input())
+print("digite outro número: ")
+numero2 = int(input())
+if numero > numero2:
+    print("O primeiro número é maior que o segundo")
+elif numero < numero2:
+    print("O segundo número é maior que o primeiro")
+else:
+    print("Os números são iguais")
