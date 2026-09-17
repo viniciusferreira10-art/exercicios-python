6de2bc0255a7c6cf5512126b774b652c5da60b43
viniciusferreira10 <vinicius_ferreira10@estudante.sesisenai.org.br>
2026-09-17 09:54:19 -0300

    exercicio 13

---
+++ parte3-while/exercicio13_.py
@@ -0,0 +1,5 @@
+contador = input("Digite sua senha: ")
+while contador != "senai123":
+    print("Senha incorreta, tente novamente.")
+    contador = input("Digite sua senha: ")
+print("Senha correta!")
